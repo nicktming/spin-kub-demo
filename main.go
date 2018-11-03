dc05c57a1bcc0ba027553b9238a7a98e29f80b80
nicktming <40271460+nicktming@users.noreply.github.com>
2018-11-03 18:43:22 +0800

    Update main.go
    update master branch

--- main.go
+++ main.go
@@ -24,5 +24,6 @@ func main() {
 	http.HandleFunc("/", index)
 	port := ":8000"
 	fmt.Printf("Starting to service on port %s\n", port);
+	fmt.Printf("Starting to service on port %s\n", port);
 	http.ListenAndServe(port, nil)
 }
